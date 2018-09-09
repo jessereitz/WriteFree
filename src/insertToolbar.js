473841b70862ed074b3bf9fecca5f621bfc3f466
Jesse Reitz <jessereitz1@gmail.com>
2018-09-09 17:26:13 -0500

    Begin fleshing out insertToolbar.

--- src/insertToolbar.js
+++ src/insertToolbar.js
@@ -1,1 +1,31 @@
+import BaseToolbar from './tb_components/base.js';
+import ToolbarButton from './tb_components/tbButton.js';
 
+const insertToolbar = Object.create(BaseToolbar);
+
+insertToolbar.init = function init(editor, options) {
+  this.initToolbar(editor, options);
+  this.createToolbarBtns();
+  return this;
+};
+
+insertToolbar.createToolbarBtns = function createToolbarBtns() {
+  this.imgBtn = Object.create(ToolbarButton);
+  this.imgBtn.init('&#128444;', 'Insert an Image', this.insertImage.bind(this), this.$btnCtn);
+  this.lineBtn = Object.create(ToolbarButton);
+  this.lineBtn.init('--', 'Insert a Horizontal Rule', this.insertLine.bind(this), this.$btnCtn);
+};
+
+insertToolbar.insertImage = function insertImage() {
+  console.log('image');
+};
+
+insertToolbar.insertLine = function insertLine() {
+  console.log('line');
+};
+
+insertToolbar.display = function display() {
+  return this.baseDisplay();
+};
+
+export default insertToolbar;
