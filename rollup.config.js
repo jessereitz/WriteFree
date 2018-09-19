02e6f72669c3b607c6d744b2ab1ba9b67d10cbb5
Jesse Reitz <jessereitz1@gmail.com>
2018-09-18 21:37:21 -0500

    Add conditional build options for rollup build process.

--- rollup.config.js
+++ rollup.config.js
@@ -4,21 +4,27 @@ import { eslint } from 'rollup-plugin-eslint';
 import babel from 'rollup-plugin-babel';
 import { uglify } from 'rollup-plugin-uglify';
 
+const plugins = [
+  eslint(),
+  babel({
+    exclude: 'node_modules/**',
+  }),
+];
+
+let sourceMap = true;
+
+if (process.env.buildTarget === 'PROD') {
+  plugins.push(uglify());
+  sourceMap = false;
+}
+
 export default {
   entry: 'src/main.js',
   output: {
     file: 'build/js/writefree.min.js',
     format: 'iife',
     name: 'WriteFree',
-    sourceMap: true,
+    sourceMap,
   },
-  // dest: 'build/js/writefree.min.js',
-  // format: 'iife',
-  plugins: [
-    eslint(),
-    babel({
-      exclude: 'node_modules/**',
-    }),
-    // uglify(),
-  ],
+  plugins,
 };
